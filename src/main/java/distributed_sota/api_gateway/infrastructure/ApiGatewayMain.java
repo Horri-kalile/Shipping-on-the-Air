c4d8d898ccd221f5dc92ca9320aded9aded65b64
AntoineOliver <antoine@LAPTOP-VSFLOUG6.localdomain>
2026-04-24 15:37:21 +0200

    docker v3

--- src/main/java/distributed_sota/api_gateway/infrastructure/ApiGatewayMain.java
+++ src/main/java/distributed_sota/api_gateway/infrastructure/ApiGatewayMain.java
@@ -1,6 +1,7 @@
 package distributed_sota.api_gateway.infrastructure;
 
 import distributed_sota.api_gateway.infrastructure.proxy.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.*;
@@ -20,22 +21,34 @@ public class ApiGatewayMain {
     }
 
     @Bean
-    public DeliveryProxy deliveryProxy(RestTemplate restTemplate) {
-        return new DeliveryProxy(restTemplate);
+    public DeliveryProxy deliveryProxy(
+            RestTemplate restTemplate,
+            @Value("${services.delivery.base-url}") String deliveryBaseUrl
+    ) {
+        return new DeliveryProxy(restTemplate, deliveryBaseUrl);
     }
 
     @Bean
-    public UserProxy userProxy(RestTemplate restTemplate) {
-        return new UserProxy(restTemplate);
+    public UserProxy userProxy(
+            RestTemplate restTemplate,
+            @Value("${services.user.base-url}") String userBaseUrl
+    ) {
+        return new UserProxy(restTemplate, userBaseUrl);
     }
 
     @Bean
-    public PaymentProxy paymentProxy(RestTemplate restTemplate) {
-        return new PaymentProxy(restTemplate);
+    public PaymentProxy paymentProxy(
+            RestTemplate restTemplate,
+            @Value("${services.payment.base-url}") String paymentBaseUrl
+    ) {
+        return new PaymentProxy(restTemplate, paymentBaseUrl);
     }
 
     @Bean
-    public DroneProxy droneProxy(RestTemplate restTemplate) {
-        return new DroneProxy(restTemplate);
+    public DroneProxy droneProxy(
+            RestTemplate restTemplate,
+            @Value("${services.dronefleet.base-url}") String dronefleetBaseUrl
+    ) {
+        return new DroneProxy(restTemplate, dronefleetBaseUrl);
     }
 }
